17cd9006a1367e95b74ad948781afcd2ab94ef75
Dylan Tivnan <tivnand@gmail.com>
2023-01-30 13:18:54 -0500

    Section 2 - DriveGame Udemy
    This is a DriveGame made in Unity from
    Complete C# Unity Game Developer 2D Course.

---
+++ Assets/Delivery.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+    
+
+    [SerializeField] float destroyDelay;
+    bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Collision Detected!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("Package Picked Up");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
+        }
+
+        if(other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Package Delivered to Customer");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+        }
+    }
+}
